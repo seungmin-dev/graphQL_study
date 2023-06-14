a24f43ded31997e0962dd958b9877399d53f31c4
seungmin-dev <seungmin.dev@gmail.com>
2023-06-14 21:47:01 +0900

    5. Query Resolvers

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ import { ApolloServer, gql } from "apollo-server";
 
 // nodemon을 사용하기 때문에 server.js를 저장할 때마다 nodemon이 서버가 재시작시킴
 
+const tweets = [
+  {
+    id: "1",
+    text: "first one",
+  },
+  {
+    id: "2",
+    text: "second one",
+  },
+];
+
 // 유저가 사용하길 원하는 모든 것들은 type Query 안에 작성되어야 함
 const typeDefs = gql`
   type User {
@@ -13,7 +24,7 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
@@ -27,7 +38,19 @@ const typeDefs = gql`
 // operation 작성할 때 default 는 query
 // mutation은 표기해줘야함
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    // tweet(root, args) {
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
