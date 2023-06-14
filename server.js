83246a28bf30cbf48fafa3b6e79b345dbb9b227c
seungmin-dev <seungmin.dev@gmail.com>
2023-06-15 00:08:56 +0900

    9. Migrating from REST to GraphQL

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import { ApolloServer, gql } from "apollo-server";
 import { error } from "console";
+import dotenv from "dotenv";
+dotenv.config();
 // package.json 하단에 "type":"module" 구문을 작성하지 않으면
 // require를 사용해서 import 하게 됨
 
@@ -43,12 +45,49 @@ const typeDefs = gql`
     allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    allMovies: [Movie!]!
+    movie(movieCd: String!): MovieDetail!
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
+  type Movie {
+    movieCd: String!
+    movieNm: String!
+    movieNmEn: String!
+    prdtYear: String!
+    openDt: String!
+    typeNm: String!
+    prdtStatNm: String!
+    nationAlt: String!
+    genreAlt: String!
+    repNationNm: String!
+    repGenreNm: String!
+  }
+  type GenreType {
+    genreNm: String!
+  }
+  type MovieDetail {
+    movieCd: String!
+    movieNm: String!
+    movieNmEn: String!
+    movieNmOg: String
+    showTm: String!
+    prdtYear: String!
+    openDt: String
+    prdtStatNm: String!
+    typeNm: String!
+    nations: [String]
+    genres: [GenreType!]!
+    directors: [String!]
+    actors: [String!]
+    showTypes: [String]
+    companys: [String!]
+  }
 `;
+// directors: [String]
+
 // operation 작성할 때 default 는 query
 // mutation은 표기해줘야함
 
@@ -63,6 +102,26 @@ const resolvers = {
     // tweet(root, args) {
     tweet(root, { id }) {
       return tweets.find((tweet) => tweet.id === id);
+    },
+    allMovies() {
+      const movieData = fetch(
+        `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${process.env.API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) =>
+          data.movieListResult.movieList.filter(
+            (movie) => movie.genreAlt !== "성인물(에로)"
+          )
+        );
+
+      return movieData;
+    },
+    movie(_, { movieCd }) {
+      return fetch(
+        `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${process.env.API_KEY}&movieCd=${movieCd}`
+      )
+        .then((res) => res.json())
+        .then((data) => data.movieInfoResult.movieInfo);
     },
   },
   Mutation: {
